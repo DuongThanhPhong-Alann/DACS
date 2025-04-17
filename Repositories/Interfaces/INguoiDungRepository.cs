2ebb7e7f448ff9ba7a92e720ae2ecc52a9031bbd
Dương Thanh Phong_2280602345 <duongthanhphong1618@gmail.com>
2025-04-17 20:56:26 +0700

    Add files via upload

---
+++ Repositories/Interfaces/INguoiDungRepository.cs
@@ -0,0 +1,17 @@
+﻿using QLCCCC.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace QLCCCC.Repositories.Interfaces
+{
+    public interface INguoiDungRepository
+    {
+        Task<List<NguoiDung>> GetAllAsync();
+        Task<NguoiDung?> GetByIdAsync(int id);
+        Task<NguoiDung?> GetByPhoneNumberAsync(string soDienThoai);
+        Task<NguoiDung?> GetByEmailAsync(string email);
+        Task AddAsync(NguoiDung nguoiDung);
+        Task UpdateAsync(NguoiDung nguoiDung);
+        Task DeleteAsync(int id);
+    }
+}
